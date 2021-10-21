ddbf35098ef6a693ff5c06e0d06acac5cbd96bb6
Ludwig <workingpeter@wp.pl>
2021-10-21 13:25:52 +0200

    Css added to popup

--- shop_2.0/pages/index.js
+++ shop_2.0/pages/index.js
@@ -1,3 +1,17 @@
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
-  return <div>Rozpoczynamy Sklep!</div>;
+  return (
+    <div className={styles.mainPage}>
+      <div className={styles.mainPage_wrapper}>
+        <div className={styles.mainPage_h1_wrapper}>
+          <h1>Welcome to React Shop!</h1>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 }
