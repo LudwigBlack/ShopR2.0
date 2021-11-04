b1e80477b624be6b4e2b298fd42d5fa3d3a0f701
Ludwig <workingpeter@wp.pl>
2021-11-04 20:27:55 +0100

    Refactoring Form with Formik

--- shop_2.0/components/Form.js
+++ shop_2.0/components/Form.js
@@ -1,3 +1,4 @@
+import { useFormik } from "formik";
 import { useProducts } from "../contexts/ProductsProvider";
 
 export const Form = () => {
@@ -10,54 +11,64 @@ export const Form = () => {
   const dispatchSpirit = useProducts().dispatch;
   const stateSpirit = useProducts().spirits;
 
-  let name;
-  let price;
-  let category;
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      price: "",
+      category: "",
+    },
 
-  function handleChangeName(e) {
-    name = e.target.value;
-  }
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+  });
 
-  function handleChangePrice(e) {
-    price = e.target.value;
-  }
+  // const whichProduct = (name, price, category) => {
+  //   const id = Math.max(...stateSnack.map((i) => i.id)) + 1;
+  //   dispatchSnack({ id, category, name, price, type: "ADD_PRODUCT" });
+  // }
 
-  function handleSelect(e) {
-    category = e.target.value;
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
+  const handleSubmit = (values) => {
+    // e.preventDefault();
+    console.log(values);
+    const { name, price, category } = values;
     if (category === "Snacks") {
       const id = Math.max(...stateSnack.map((i) => i.id)) + 1;
-
       dispatchSnack({ id, category, name, price, type: "ADD_PRODUCT" });
     } else if (category === "Drinks") {
       const id = Math.max(...stateDrink.map((i) => i.id)) + 1;
-
       dispatchDrink({ id, category, name, price, type: "ADD_PRODUCT" });
     } else if (category === "Spirits") {
       const id = Math.max(...stateSpirit.map((i) => i.id)) + 1;
-
       dispatchSpirit({ id, category, name, price, type: "ADD_PRODUCT" });
     } else {
       console.log("coś nie działa");
     }
-  }
+  };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <input
-        name="product name"
+        id="name"
+        name="name"
         type="text"
         placeholder="Enter product name"
-        onChange={handleChangeName}
+        onChange={formik.handleChange}
+        value={formik.values.name}
       />
       <input
+        id="price"
+        name="price"
         type="number"
         placeholder="Enter product price"
-        onChange={handleChangePrice}
+        onChange={formik.handleChange}
+        value={formik.values.price}
       />
-      <select onChange={handleSelect}>
+      <select
+        id="category"
+        name="category"
+        onChange={formik.handleChange}
+        value={formik.values.category}
+      >
         <option>Wybierz rodzaj produktu</option>
         <option value="Snacks">Snacks</option>
         <option value="Drinks">Drinks</option>
